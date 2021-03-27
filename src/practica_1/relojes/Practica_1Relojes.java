a5c1c19352b9606ec64fac3b97c8ae9d15503678
ale_j <ale_j@LAPTOP-C7VHKPHL>
2021-03-27 00:02:36 -0600

    Versión del 27/03/2021

---
+++ src/practica_1/relojes/Practica_1Relojes.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica_1.relojes;
+
+/**
+ *
+ * @author ale_j
+ */
+public class Practica_1Relojes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
